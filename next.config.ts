be1d4a65ad6255e460f671b979f787036f8b63bb
Jamppa <132147167+Jambovisuaalit@users.noreply.github.com>
2026-08-30 20:36:26 +0300

    chore: use local website assets

--- next.config.ts
+++ next.config.ts
@@ -3,14 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   poweredByHeader: false,
   reactStrictMode: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "raw.githubusercontent.com",
-      },
-    ],
-  },
 };
 
 export default nextConfig;
